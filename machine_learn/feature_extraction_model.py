130c337102ea1abc21733dffead6e608d5bb0357
tianyuningmou <tianyuningmou2009@126.com>
2018-03-08 19:03:12 +0800

    feature_extraction

---
+++ machine_learn/feature_extraction_model.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: feature_extraction_model.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/3/7 下午4:13
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/3/7 下午4:13
+"""
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
