429e5b1df3f5c2e7f95c2f003c34e5bdcc830d7f
tianyuningmou <tianyuningmou2009@126.com>
2018-04-19 16:06:48 +0800

    __init__

---
+++ commonly_package/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: __init__.py.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/4/19 下午4:04
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/4/19 下午4:04
+"""
+
+
